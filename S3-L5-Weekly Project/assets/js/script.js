c9c8d758030d697c20efb0c53a292b2e0308c0d9
Globulo <109812198+GlobuloBianco@users.noreply.github.com>
2022-10-14 13:13:48 +0200

    Add files via upload

---
+++ S3-L5-Weekly Project/assets/js/script.js
@@ -0,0 +1,47 @@
+const display = document.getElementById("display");
+
+// Input dei numeri e operandi
+scrivi = (lettera, numero) => {
+        document.getElementById(lettera).onclick = function() {
+        display.value += numero;
+    }
+}
+
+display.innerHTML = scrivi("zero", "0");
+display.innerHTML = scrivi("uno", "1");
+display.innerHTML = scrivi("due", "2");
+display.innerHTML = scrivi("tre", "3");
+display.innerHTML = scrivi("quattro", "4");
+display.innerHTML = scrivi("cinque", "5");
+display.innerHTML = scrivi("sei", "6");
+display.innerHTML = scrivi("sette", "7");
+display.innerHTML = scrivi("otto", "8");
+display.innerHTML = scrivi("nove", "9");
+display.innerHTML = scrivi("punto", ".");
+display.innerHTML = scrivi("piu", "+");
+display.innerHTML = scrivi("meno", "-");
+display.innerHTML = scrivi("per", "*");
+display.innerHTML = scrivi("diviso", "/");
+ 
+// All Clear
+document.getElementById('ac').onclick = function() {
+    display.value = '';
+}
+
+// Somma + decimali arrotondati
+document.getElementById("uguale").onclick = function() {
+    let risultato = Math.round(eval(display.value) * 100) / 100;
+    display.value = ''; // per togliere l'operazione prima di mettere il risultato
+    display.value += risultato;
+}
+
+// Delete
+document.getElementById('del').onclick = function() {
+    display.value = display.value.slice(0, -1);;
+}
+
+// Btn animazione
+document.getElementById('lol').onclick = function() {
+    window.location.reload();
+}
+//sono stato tipo 1 ora quasi per fare l'animazione... dopo diversi tentativi ci ho rinunciato facendolo fare col refresh.
